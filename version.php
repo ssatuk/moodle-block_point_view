94a0a2eaa0c59f1428f381ecb4fbbb0734214c6f
Astor Bizard <astor.bizard@univ-grenoble-alpes.fr>
2024-04-11 12:46:35 +0200

    Version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_point_view';
-$plugin->version   = 2022112200;
-$plugin->requires  = 2017051502;
+$plugin->version   = 2024041100;
+$plugin->requires  = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.7';
+$plugin->release = '2.0 for Moodle 4';
